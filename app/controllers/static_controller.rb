943160f97964d50342f44229cb0b8278e6f6e8f3
Jon Pahl <jdpahl122@gmail.com>
2023-04-21 10:56:35 -0400

    Standardrb fix

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
-  require 'sendgrid-ruby'
+  require "sendgrid-ruby"
   include SendGrid
   def index
   end
@@ -8,7 +8,7 @@ class StaticController < ApplicationController
   end
 
   def pricing
-    redirect_to root_path, alert: t('.no_plans_html', link: helpers.link_to_if(current_user&.admin?, 'Add a plan in the admin', admin_plans_path)) unless Plan.without_free.exists?
+    redirect_to root_path, alert: t(".no_plans_html", link: helpers.link_to_if(current_user&.admin?, "Add a plan in the admin", admin_plans_path)) unless Plan.without_free.exists?
 
     plans = Plan.visible.sorted
     @monthly_plans = plans.select(&:monthly?)
@@ -27,16 +27,16 @@ class StaticController < ApplicationController
     sg = SendGrid::API.new(api_key: Rails.application.credentials[:sendgrid_contact_list][:api_key])
     list_id = Rails.application.credentials[:sendgrid_contact_list][:list_id]
     email = params[:email]
-    data = { list_ids: [list_id], contacts: [{ email: email }] }.to_json
+    data = {list_ids: [list_id], contacts: [{email: email}]}.to_json
     begin
       response = sg.client.marketing.contacts.put(request_body: data)
-    rescue StandardError => e
+    rescue => e
       puts e.message
     else
       puts response.status_code
       puts response.headers
       puts response.body
-      flash[:notice] = 'Thank you for your interest. We will be in touch soon.'
+      flash[:notice] = "Thank you for your interest. We will be in touch soon."
       redirect_to root_path
     end
   end
