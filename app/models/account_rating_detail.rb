6df18e76901241ee51a501729d6bda6bb9cb5ae4
Jon Pahl <jdpahl122@gmail.com>
2023-04-25 21:58:45 -0400

    Update rating details on update

--- app/models/account_rating_detail.rb
+++ app/models/account_rating_detail.rb
@@ -25,7 +25,13 @@
 class AccountRatingDetail < ApplicationRecord
   belongs_to :account
   # Broadcast changes in realtime with Hotwire
-  after_create_commit -> { broadcast_prepend_later_to :account_rating_details, partial: "account_rating_details/index", locals: {account_rating_detail: self} }
-  after_update_commit -> { broadcast_replace_later_to self }
-  after_destroy_commit -> { broadcast_remove_to :account_rating_details, target: dom_id(self, :index) }
+  # after_create_commit -> { broadcast_prepend_later_to :account_rating_details, partial: "account_rating_details/index", locals: {account_rating_detail: self} }
+  # after_update_commit -> { broadcast_replace_later_to self }
+  # after_destroy_commit -> { broadcast_remove_to :account_rating_details, target: dom_id(self, :index) }
+
+  after_update do
+    if self.total_ratings.to_i > 0
+      self.update_column(:total_ratings_score, ((self.one_star_count.to_i * 1) + (self.two_star_count.to_i * 2) + (self.three_star_count.to_i * 3) + (self.four_star_count.to_i * 4) + (self.five_star_count.to_i * 5)) / self.total_ratings.to_i)
+    end
+  end
 end
--- test/models/account_rating_detail_test.rb
+++ test/models/account_rating_detail_test.rb
@@ -25,7 +25,41 @@
 require "test_helper"
 
 class AccountRatingDetailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @target_account = accounts(:company)
+    @source_account = accounts(:one)
+    AccountRating.create(source_account_id: @source_account.id, target_account_id: @target_account.id, rating: 5, title: "Test1", review: "Test")
+    AccountRating.create(source_account_id: @source_account.id, target_account_id: @target_account.id, rating: 5, title: "Test2", review: "Test")
+    AccountRating.create(source_account_id: @source_account.id, target_account_id: @target_account.id, rating: 5, title: "Test3", review: "Test")
+  end
+
+  test "has the correct average score" do
+    @account_rating_detail = account_rating_details(:one)
+    @maths = (((@account_rating_detail.five_star_count * 5) + (@account_rating_detail.four_star_count * 4) + (@account_rating_detail.three_star_count * 3) + (@account_rating_detail.two_star_count * 2) + (@account_rating_detail.one_star_count * 1)).to_f / @account_rating_detail.total_ratings).to_f
+    assert_equal @maths, @account_rating_detail.total_ratings_score
+  end
+
+  test "has the incorrect average score" do
+    @account_rating_detail = account_rating_details(:two)
+    @maths = (((@account_rating_detail.five_star_count * 5) + (@account_rating_detail.four_star_count * 4) + (@account_rating_detail.three_star_count * 3) + (@account_rating_detail.two_star_count * 2) + (@account_rating_detail.one_star_count * 1)).to_f / @account_rating_detail.total_ratings).to_f
+    assert_not_equal @maths, @account_rating_detail.total_ratings_score
+  end
+
+  test "Account ratings detail correctly updates on new ratings" do
+    assert_equal 3, @target_account.account_rating_detail.total_ratings.to_i
+    assert_equal 3, @target_account.account_rating_detail.five_star_count.to_i
+    assert_equal 0, @target_account.account_rating_detail.four_star_count.to_i
+    assert_equal 0, @target_account.account_rating_detail.three_star_count.to_i
+    assert_equal 0, @target_account.account_rating_detail.two_star_count.to_i
+    assert_equal 0, @target_account.account_rating_detail.one_star_count.to_i
+  end
+
+  test "Account total score correctly updates on new ratings" do
+    assert_equal 5, @target_account.account_rating_detail.total_ratings_score.to_i
+    AccountRating.create(source_account_id: @source_account.id, target_account_id: @target_account.id, rating: 1, title: "Test2", review: "Test")
+    AccountRating.create(source_account_id: @source_account.id, target_account_id: @target_account.id, rating: 1, title: "Test2", review: "Test")
+    AccountRating.create(source_account_id: @source_account.id, target_account_id: @target_account.id, rating: 1, title: "Test2", review: "Test")
+    @target_account.account_rating_detail.reload
+    assert_equal 3, @target_account.account_rating_detail.total_ratings_score.to_i
+  end
 end
