8a164146b104dde2f010084eeb46ad1909867e1e
Jon Pahl <jdpahl122@gmail.com>
2023-05-11 09:18:29 -0400

    Fix issues with map loading

--- app/javascript/controllers/user_map_controller.js
+++ app/javascript/controllers/user_map_controller.js
@@ -8,14 +8,19 @@ export default class extends Controller {
     if (typeof (google) != "undefined") {
       this.initializeMap()
     }
+    else {
+      window.addEventListener("google-maps-callback", () => {
+        this.initializeMap()
+      })
+    }
   }
 
   async initializeMap() {
     //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps");
+    const {Map} = await google.maps.importLibrary("maps");
 
     const map = new Map(this.mapTarget, {
-      center: { lat: 40.7328, lng: -73.9991 },
+      center: {lat: 40.7328, lng: -73.9991},
       zoom: 14,
     });
   }
