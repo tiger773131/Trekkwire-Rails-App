b8a899d0231d5a6349e769dd74d2d83ce48a7ed0
Jon Pahl <jdpahl122@gmail.com>
2023-05-13 09:14:24 -0400

    Add careers page and route

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -7,6 +7,9 @@ class StaticController < ApplicationController
   def about
   end
 
+  def careers
+  end
+
   def pricing
     redirect_to root_path, alert: t(".no_plans_html", link: helpers.link_to_if(current_user&.admin?, "Add a plan in the admin", admin_plans_path)) unless Plan.without_free.exists?
 
