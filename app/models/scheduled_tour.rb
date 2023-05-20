8f6d5f1f5a336cd250fefc3d7c1b6be209d75634
Jon Pahl <jdpahl122@gmail.com>
2023-05-20 16:45:17 -0400

    run migration

--- app/models/scheduled_tour.rb
+++ app/models/scheduled_tour.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: scheduled_tours
+#
+#  id              :bigint           not null, primary key
+#  location        :string
+#  scheduled_date  :date
+#  scheduled_time  :time
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  account_user_id :bigint           not null
+#  tour_id         :bigint           not null
+#
+# Indexes
+#
+#  index_scheduled_tours_on_account_user_id  (account_user_id)
+#  index_scheduled_tours_on_tour_id          (tour_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (account_user_id => account_users.id)
+#  fk_rails_...  (tour_id => tours.id)
+#
 class ScheduledTour < ApplicationRecord
   belongs_to :tour
   belongs_to :account_user
