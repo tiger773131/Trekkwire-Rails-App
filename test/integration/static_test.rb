ad8fa6fee2ef468d541a015d3ab0deee67df63da
Jon Pahl <jdpahl122@gmail.com>
2023-07-01 18:58:57 -0400

    Fix broken test

--- test/integration/static_test.rb
+++ test/integration/static_test.rb
@@ -9,6 +9,6 @@ class Jumpstart::StaticTest < ActionDispatch::IntegrationTest
   test "dashboard" do
     sign_in users(:one)
     get root_path
-    assert_select "h1", I18n.t("dashboard.show.title_traveler")
+    assert_select "h3", I18n.t("dashboard.show.title_traveler")
   end
 end
