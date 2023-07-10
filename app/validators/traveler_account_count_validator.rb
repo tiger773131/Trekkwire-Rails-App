e8f2ea4e4390ac706e72d89c1416e3e4a3a82d29
Jon Pahl <jdpahl122@gmail.com>
2023-07-09 20:48:23 -0400

    Fix linting

--- app/validators/traveler_account_count_validator.rb
+++ app/validators/traveler_account_count_validator.rb
@@ -3,7 +3,7 @@ class TravelerAccountCountValidator < ActiveModel::Validator
   # unless they have approval
   #
   def validate(record)
-    if (record.owner.present? && record.owner.accounts.count > 0 && !record.owner.admin? && !record.owner.account_creator)
+    if record.owner.present? && record.owner.accounts.count > 0 && !record.owner.admin? && !record.owner.account_creator
       record.errors.add :base, "You are only allowed to create a single account."
     end
   end
