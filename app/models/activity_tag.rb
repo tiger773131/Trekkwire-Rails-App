e4690fbc984becdc592bdc3869c89b7d153eb260
Jon Pahl <jdpahl122@gmail.com>
2023-09-12 19:39:28 -0400

    Add has_many mappings

--- app/models/activity_tag.rb
+++ app/models/activity_tag.rb
@@ -12,4 +12,7 @@ class ActivityTag < ApplicationRecord
   after_create_commit -> { broadcast_prepend_later_to :activity_tags, partial: "activity_tags/index", locals: {activity_tag: self} }
   after_update_commit -> { broadcast_replace_later_to self }
   after_destroy_commit -> { broadcast_remove_to :activity_tags, target: dom_id(self, :index) }
+
+  has_many :account_activity_taggings, dependent: :destroy
+  has_many :accounts, through: :account_activity_taggings
 end
