4d3e06ea671f6d4a1a8788350394371e9f89f49a
Jon Pahl <jdpahl122@gmail.com>
2023-09-25 23:07:41 -0400

    Update admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -8,16 +8,19 @@ module Admin
       @last_month = last_month
       @this_month = this_month
       @users = ::User.all.count
+      @users_last_month = users_last_month
+      @users_last_12_mos = users_last_12_mos
+      @users_this_month = users_this_month
       @guide_accounts = ::Account.where(customer_type: "guide").count
       @traveler_accounts = ::Account.where(customer_type: "traveler").count
       @scheduled_tours = ::ScheduledTour.where(paid: true).count
-      @subscriptions = ::Pay::Subscription.active.count
     end
 
     def total_revenue
-      revenue_in_cents = ::Pay::Charge.sum(:amount)
-      refunds_in_cents = ::Pay::Charge.sum(:amount_refunded)
-      (revenue_in_cents - refunds_in_cents) / 100.0
+      # revenue_in_cents = ::Pay::Charge.sum(:amount)
+      # refunds_in_cents = ::Pay::Charge.sum(:amount_refunded)
+      # (revenue_in_cents - refunds_in_cents) / 100.0
+      ScheduledTour.where(paid: true).sum(:total_paid)
     end
 
     def last_12_mos
@@ -34,10 +37,26 @@ module Admin
       revenue_for_range month.beginning_of_month..month.end_of_month
     end
 
+    def users_last_month
+      users_for_range Time.current.prev_month.beginning_of_month..Time.current.prev_month.end_of_month
+    end
+
+    def users_this_month
+      users_for_range Time.current.beginning_of_month..Time.current.end_of_month
+    end
+
+    def users_last_12_mos
+      users_for_range 12.months.ago..Time.current
+    end
     def revenue_for_range(range)
-      revenue_in_cents = ::Pay::Charge.where(created_at: range).sum(:amount)
-      refunds_in_cents = ::Pay::Charge.where(created_at: range).sum(:amount_refunded)
-      (revenue_in_cents - refunds_in_cents) / 100.0
+      # revenue_in_cents = ::Pay::Charge.where(created_at: range).sum(:amount)
+      # refunds_in_cents = ::Pay::Charge.where(created_at: range).sum(:amount_refunded)
+      # (revenue_in_cents - refunds_in_cents) / 100.0
+      ScheduledTour.where(paid: true, created_at: range).sum(:total_paid)
+    end
+
+    def users_for_range(range)
+      ::User.where(created_at: range).count
     end
   end
 end
