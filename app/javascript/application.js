6b5097a0a625f46d45e61811ed560eacabb60fff
Jon Pahl <jdpahl122@gmail.com>
2023-09-26 17:58:36 -0400

    Add analytics tags

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -27,3 +27,9 @@ window.dispatchMapsEvent = function (...args) {
   const event = new Event("google-maps-callback", { bubbles: true, cancelable: true });
   window.dispatchEvent(event)
 }
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-T0GLNCRC2J');
